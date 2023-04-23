d7333ce2e81a706c6790cac659ad2b02ae46b308
Honkato <77444910+Honkato@users.noreply.github.com>
2023-04-23 10:32:41 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask('_name_')
+
+
+# with open('data.json', 'r') as arquivo:
+#     livros = json.load(arquivo)
+
+
+livros = [
+  {
+    "_id": {
+      "$oid": "642a14682f362b1354778e32"
+    },
+    "quantidade": 2,
+    "cor": "vermelho"
+  },
+  {
+    "_id": {
+      "$oid": "642b55fdf87413a323baffa8"
+    },
+    "quantidade": 2,
+    "cor": "azul"
+  }
+]
+
+@app.route('/livros', methods=['GET'])
+def obter_livro():
+    return jsonify(livros)
+
+
+app.run(port=5000,host='localhost',debug=True)
